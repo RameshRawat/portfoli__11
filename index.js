096c42e86776f5abeffa22d6f18c011295303aed
tham <tham@podamibenepal.com>
2022-06-09 13:25:12 +0545

    nav finished

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const hamburg = document.querySelector(".icon");
 const menuContainer = document.querySelector(".container");
+const iconBars = document.querySelector(".fa-bars");
 
 hamburg.addEventListener("click", (e) => {
   menuContainer.classList.toggle("d-none");
+  iconBars.classList.toggle("fa-times");
+  iconBars.classList.toggle("fa-bars");
 });
